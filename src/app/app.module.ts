7ef8a2e2d72b6c020da1fb49d2908d3c15dc57b0
vivekmalik <vmvivekmalik91@gmail.com>
2018-02-02 18:22:04 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import {RouterModule,Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { AboutComponent } from './components/about/about.component';
+import { DataService } from './services/data.service';
 
+const appRoutes  : Routes = [
+    {path:'',component:UsersComponent},
+    {path:'about',component:AboutComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
